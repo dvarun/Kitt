178115dc9d4c276c9a622ff0511ad94d5bf69c72
Varun Dubey <dvarun@hotmail.com>
2015-08-23 14:48:50 +0700

    preparing for deploy later

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,8 +7,6 @@ end
  def create
   @video = Video.new(video_params)
   @video.user_id = current_user.id
-  @video.file_path = @video.content.url
-  #@video.file_size = video_file_size
   if @video.save
     #redirect_to project_path(@task.project_id), notice: "Successfully added new task"
     redirect_to batch_path(@video.batch_id), notice: "successfully added new video"
@@ -19,7 +17,7 @@ end
 
  private
  def video_params
-   params[:video].permit(:name,:batch_id,:content)
+   params[:video].permit(:name,:batch_id,:file_path)
  end
 
 end
