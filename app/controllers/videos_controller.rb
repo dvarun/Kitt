0277506fffe59258bb7567cb5b0d54f9e6cecb8e
Varun Dubey <dvarun@hotmail.com>
2015-10-14 19:22:27 +0700

    fix video validation for admin, fixed comment box css

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -15,7 +15,7 @@ class VideosController < ApplicationController
   @video = Video.new(video_params)
   @video.user_id = current_user.id
   if @video.save
-     #@video.convert
+   #@video.convert
    #redirect_to project_path(@task.project_id), notice: "Successfully added new task"
    redirect_to batch_path(@video.batch_id), notice: "successfully added new video"
   else
@@ -24,26 +24,28 @@ class VideosController < ApplicationController
  end
 
  def show
-   @video = Video.find(params[:id])
+  @video = Video.find(params[:id])
 
-   @batch_user = BatchUser.where("user_id = ? and batch_id = ?",current_user.id,@video.batch_id)
+  @batch_user = BatchUser.where("user_id = ? and batch_id = ?",current_user.id,@video.batch_id)
+  if !is_admin?
    if @batch_user.empty?
     redirect_to root_url,notice: "The Video doesn't exist"
    end
+  end
 
-   #add user detail who wathed this video
-   @check_user = WatchedVideo.where("user_id = ? and video_id=? and batch_id=?",current_user.id,@video.id,@video.batch_id)
-   if @check_user.empty?
-    @watched_video = WatchedVideo.new
-    @watched_video.user_id = current_user.id
-    @watched_video.video_id = @video.id
-    @watched_video.batch_id = @video.batch_id
-    @watched_video.save
-   end
-   #end add user detail
-   @comment = Comment.new
-   @comments = Comment.where("video_id = ?",@video.id)
-   @videos = Video.where("batch_id = ?",@video.batch_id)
+  #add user detail who wathed this video
+  @check_user = WatchedVideo.where("user_id = ? and video_id=? and batch_id=?",current_user.id,@video.id,@video.batch_id)
+  if @check_user.empty?
+   @watched_video = WatchedVideo.new
+   @watched_video.user_id = current_user.id
+   @watched_video.video_id = @video.id
+   @watched_video.batch_id = @video.batch_id
+   @watched_video.save
+  end
+  #end add user detail
+  @comment = Comment.new
+  @comments = Comment.where("video_id = ?",@video.id)
+  @videos = Video.where("batch_id = ?",@video.batch_id)
  end
 
  def destroy
@@ -57,17 +59,17 @@ class VideosController < ApplicationController
 
 
  def edit
-   @video = Video.find(params[:id])
+  @video = Video.find(params[:id])
  end
 
  def update
-   @video = Video.find(params[:id])
-   if @video.update_attributes(video_params)
-     #redirect_to clients_path, notice: "Successfully updated the task detail for the client"
-     redirect_to batch_path(@video.batch_id), notice: "successfully updated Video"
-   else
-     render action:"edit"
-   end
+  @video = Video.find(params[:id])
+  if @video.update_attributes(video_params)
+   #redirect_to clients_path, notice: "Successfully updated the task detail for the client"
+   redirect_to batch_path(@video.batch_id), notice: "successfully updated Video"
+  else
+   render action:"edit"
+  end
  end
 
 
